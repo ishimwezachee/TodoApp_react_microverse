e70a4f050d1a4da0d55ac5869dc0abe0c953ab76
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-07 15:39:29 +0200

    Add input method

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,27 +1,29 @@
 import React from "react";
 import TodoList from "./TodoList";
 import Header from "./Header";
+import InputTodo from "./InputTodo";
+import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Setup development environment",
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Develop website and add content",
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Deploy to live server",
         completed: false,
       },
     ],
   };
-
+ 
   // write the class method
   handleChange = (id) => {
     this.setState(prev=>({
@@ -38,13 +40,29 @@ class TodoContainer extends React.Component {
   };
 
   deleteTodo = id =>{
-    console.log('deleted',id);
+    this.setState({
+      todos:[
+        ...this.state.todos.filter(todo=> todo.id !== id)
+      ]
+    })
   }
-  
+ 
+  addTodoItem = title =>{
+    const newTodo = {
+      id:uuidv4(),
+      title:title,
+      completed:false
+    };
+    this.setState({
+      todos:[...this.state.todos,newTodo]
+    })
+  }
+
   render() {
     return (
       <div>
         <Header />
+        <InputTodo addTodoProps ={this.addTodoItem}/>
         <TodoList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
