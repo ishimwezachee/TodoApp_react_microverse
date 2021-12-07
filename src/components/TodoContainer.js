58d4d766ebade01423b9c69aa236713199e9090f
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-07 21:47:24 +0200

    Apply componentDidMount method for react life cycle to handle the sideEffect

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,25 +5,15 @@ import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
- 
+  
+  // after data rendered 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(response => response.json())
+    .then(data=> this.setState({todos:data}))
+  }
   // write the class method
   handleChange = (id) => {
     this.setState(prev=>({
