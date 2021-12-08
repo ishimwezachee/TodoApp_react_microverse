e43e699cd67dd840a72b39ab3b16a24cacc2ae73
ishimwezachee <zacheeishimwe@gmail.com>
2021-12-08 15:21:13 +0200

    Add react routers to connect component with their specific nav

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom"
 // component file
 import TodoContainer from "./functionBased/components/TodoContainer";
 
@@ -7,7 +8,9 @@ import TodoContainer from "./functionBased/components/TodoContainer";
 import "./functionBased/App.css";
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
     <TodoContainer />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
